3d4453094902f24df2d81ddc342d3d747aec76fb
231130494 <167203628+231130494@users.noreply.github.com>
2025-07-07 23:10:03 +0700

    latest vers

--- exchange_rate_service.dart
+++ exchange_rate_service.dart
@@ -8,27 +8,25 @@ class ExchangeRateService {
 
   Future<double?> fetchExchangeRate(String targetCurrency) async {
     try {
-      // Pastikan _apiKey tidak kosong
       if (_apiKey == 'YOUR_API_KEY' || _apiKey.isEmpty) {
         print('ExchangeRateService: API Key belum diatur. Harap daftar di exchangerate-api.com untuk mendapatkan kunci API.');
         return null;
       }
 
-      final response = await http.get(Uri.parse('$_baseUrl$_apiKey/latest/IDR')); // Mengambil data dengan base IDR
-
+      final response = await http.get(Uri.parse('$_baseUrl$_apiKey/latest/IDR')); 
       if (response.statusCode == 200) {
         final Map<String, dynamic> data = json.decode(response.body);
         
-        // Periksa apakah permintaan berhasil
-        if (data['result'] == 'success') { // Periksa properti 'result' untuk keberhasilan
-          if (data.containsKey('conversion_rates') && data['conversion_rates'].containsKey(targetCurrency)) { // Akses 'conversion_rates'
-            return (data['conversion_rates'][targetCurrency] as num).toDouble(); // Ambil kurs dari 'conversion_rates'
+       
+        if (data['result'] == 'success') { 
+          if (data.containsKey('conversion_rates') && data['conversion_rates'].containsKey(targetCurrency)) { 
+            return (data['conversion_rates'][targetCurrency] as num).toDouble();
           } else {
             print('ExchangeRateService: Kurs mata uang target "$targetCurrency" tidak ditemukan.');
             return null;
           }
         } else {
-          print('ExchangeRateService: API Error - ${data['error-type']}'); // Tangani kesalahan API spesifik
+          print('ExchangeRateService: API Error - ${data['error-type']}'); 
           return null;
         }
       } else {
